ef4d32d5043fbe1b23d044cec98b3ffb32a6944a
Eilish Matteson <e.matteson18@gmail.com>
2024-11-24 08:13:33 -0700

    test

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const navElement = document.querySelector ('#theNav');
+const btnElement = document.querySelector ('#theButton');
+
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');
+    navElement.classList.toggle('open');
+});
+
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // start AutoPlay 
+    autoplay: {
+        delay: 5000,
+},
+  });
